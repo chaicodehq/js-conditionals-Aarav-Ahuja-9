f0a664d9150ebe8d5d48b48ff93f68a0569e76c9
Aarav-Ahuja-9 <mr.aaravahuja@gmail.com>
2026-02-12 23:38:38 +0530

    My third commit for my third assignment completed it is fun and easy to complete

--- src/03-grade-calculator.js
+++ src/03-grade-calculator.js
@@ -25,5 +25,30 @@
  * @returns {string} The letter grade or "INVALID"
  */
 export function calculateGrade(score, hasExtraCredit) {
-  // Your code here
+  if (score < 0 || score > 100) {
+    return "Invalid"
+  }
+
+  // The conditions only need true or false values to procced and the variable "hasExtraCredit" is boolean which will return false or true so i didn't add the hasExtraCredit == true
+  if (hasExtraCredit) { 
+    score += 5;
+  }
+
+  if (score > 100) {
+    score = 100;
+  }
+
+  if (score >= 0 && score < 60) {
+    return "F"
+  } else if (score >= 60 && score < 70) {
+    return "D"
+  } else if (score >= 70 && score < 80) {
+    return "C"
+  } else if (score >= 80 && score < 90) {
+    return "B"
+  } else if (score >= 90 && score <= 100) {
+    return "A"
+  } 
 }
+
+console.log(calculateGrade(98, true))
